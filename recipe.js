962022f45ca64cfd5a60d973aa73ba6b994c088e
ijgreenidge <igreenidge2018@bklawtech.com>
2017-01-03 22:54:11 +0000

    canadadry

---
+++ recipe.js
@@ -0,0 +1,11 @@
+var recipe = {
+    servings : 1 ,
+    
+    recipeTitle :  "Canada Dry" , 
+    
+     ingredients : ["Carbonated_Water", "Citric_Acid", "Natural_Flavors"]
+
+ 
+} 
+    
+console.log(recipe.ingredients)
